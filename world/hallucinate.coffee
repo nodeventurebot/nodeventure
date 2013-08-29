5589e7191aa0786725da1267e43c1d8f897f5ac9
Simon Elliott <simon@papercreatures.com>
2013-08-29 21:16:45 +0100

    clean up fun

--- world/hallucinate.coffee
+++
@@ -1,8 +0,0 @@
-itemCommand 'use', 'mirror', (rest, player, game) ->
-  if player.mirrored
-    player.mirrored = false
-    player.write {effect: "unmirror"}
-  else
-    player.mirrored = true
-    player.write {effect: "mirror"}
-    player.write "Hi, Sexy"
--- world/help_commands.js
+++ world/help_commands.js
@@ -3,28 +3,14 @@ command('commands', 'list all commands available in the game', function (rest, p
 });
 
 command('help', 'get you some help! Example: "help <command>"', function (rest, player, game) {
-	if (rest) {
-	    var command = game.commands[rest];
-	    if (command) {
-	      player.write(rest + ': ' + command.description);
-	    } else {
-	      player.write("I don't recognise that command");
-	    }
-	  } else {
-	    player.write('Try typing "commands" to get a list of commands then "help <command>" for more info.\nSome other thngs to try: "go <direction>" to move, "say <message>" to talk and "look" to look around');
-	  }
-});
-
-command('roll', 'Broadcast a dice roll between 1-6: "roll". See also "dice".', function (rest, player, game) {
-  player.execute("dice");
-});
-
-command('dice', 'Broadcast a dice roll between 1-6: "dice". See also "roll"', function (rest, player, game) {
-  var roll = Math.ceil(Math.random()*6);
-  player.getCurrentRoom().broadcast(player.name + ' rolls a ' + roll, player);
-  player.write('You rolled a ' + roll);
-});
-
-command('zyzzy', 'Will anything happen?', function (rest, player, game) {
-  player.write('Nothing happens');
+  if (rest) {
+      var command = game.commands[rest];
+      if (command) {
+        player.write(rest + ': ' + command.description);
+      } else {
+        player.write("I don't recognise that command");
+      }
+    } else {
+      player.write('Try typing "commands" to get a list of commands then "help <command>" for more info.\nSome other thngs to try: "go <direction>" to move, "say <message>" to talk and "look" to look around');
+    }
 });
