74084263d9256c50669617f4b39f688a4b509e84
Dave Gurnell <dave@untyped.com>
2013-08-30 07:18:06 +0100

    Rename "spawnFrequency" to "respawnTimer"; remove "items" keys from room definitions

--- world/home.js
+++ world/home.js
@@ -3,28 +3,27 @@ room('home', {
     A large table sits in the middle of the room around which \
     geeks sit hard at work on some kind of text adventure...",
   image: "https://si0.twimg.com/profile_images/2346548968/v670k756qsd1e3hegurf.png",
-  exits: { west: 'beige', east: 'magnolia' },
-  items: []});
+  exits: { west: 'beige', east: 'magnolia' }
+});
 
 room('beige', {
   description: "This room is daubed in beige. You start feeling sleepy.",
-  exits: { east: 'home' },
-  items: []
+  exits: { east: 'home' }
 });
 
 room('magnolia', {
   description: "This room is daubed in magnolia. You stare at it for a while.",
-  exits: { west: 'home' },
+  exits: { west: 'home' }
 });
 
-item('beige', 'brush', 60, {
+item('beige', 'brush', {
+  respawnTimer: 60,
   short: 'a brush',
-  name: 'brush',
   description: 'A brush. it is branded with a \'B&Q\' logo. There is beige paint on it'
 });
 
-item('beige', 'paint', 60, {
+item('beige', 'paint', {
+  respawnTimer: 60,
   short: 'a pot of paint',
-  name: 'paint',
   description: 'A pot of beige paint. it has been opened.'
 });
